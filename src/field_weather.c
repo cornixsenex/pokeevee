aade08ad743f84a6e57503c99a5cdfde24e62007
ironjohnson <phallusferrus@toke.com>
2021-03-23 20:00:40 -0700

    fixed weather palettes

--- src/field_weather.c
+++ src/field_weather.c
@@ -154,7 +154,8 @@ void StartWeather(void)
 {
     if (!FuncIsActiveTask(Task_WeatherMain))
     {
-        u8 index = AllocSpritePalette(0x1200);
+        //u8 index = AllocSpritePalette(0x1200);
+		u8 index = 15;
         CpuCopy32(gFogPalette, &gPlttBufferUnfaded[0x100 + index * 16], 32);
         BuildGammaShiftTables();
         gWeatherPtr->altGammaSpritePalIndex = index;
