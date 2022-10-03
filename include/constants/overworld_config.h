61c5f4c0ae312f7d152cbb4501e42dbdb185b890
cornixsenex <phallusferrus@toke.com>
2022-10-03 00:44:56 -0700

    updated poryscript and set debug menu hold button to L

--- include/constants/overworld_config.h
+++ include/constants/overworld_config.h
@@ -7,7 +7,7 @@
 
 // Debug options
 #define DEBUG_SYSTEM_ENABLE         TRUE               // Enables a overworld debug menu for changing flags, variables, giving pokemon and more, accessed by holding R and pressing START while in the overworld by default.
-#define DEBUG_SYSTEM_HELD_KEYS      (R_BUTTON)         // The keys required to be held to open the debug menu.
+#define DEBUG_SYSTEM_HELD_KEYS      (L_BUTTON)         // The keys required to be held to open the debug menu.
 #define DEBUG_SYSTEM_TRIGGER_EVENT  pressedStartButton // The event that opens the menu when holding the key(s) defined in DEBUG_SYSTEM_HELD_KEYS.
 #define DEBUG_SYSTEM_IN_MENU        FALSE              // Replaces the overworld debug menu button combination with a start menu entry (above Pokédex).
 
