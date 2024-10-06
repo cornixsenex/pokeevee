110ca7eeee622d3cf73b34cd36e8aa27f3a68950
cornixsenex <phallusferrus@toke.com>
2024-10-06 00:32:01 -0700

    work on Colchis Intro

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -146,8 +146,6 @@
 																//6: After Prometheus unchained and battle (Cave ready for Spearow release)
 																//7: Cave opened; Spearow released; Setup colchis intro script; Jason in Colchis 
 																//8: Colchis introd
-																//9: Colchis Forced Battle state (Jason at entrance)
-																//10: Colchis Enter Throne Room
 #define VAR_IGNISMONS_STATE                              0x4072 // Controls Ignis Mons Color Puzzle
 																// 0: Blank
 																// 1: Red
