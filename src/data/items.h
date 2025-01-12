53e3a29b4d8edbde4ffbb4e1d6a2d7bccf8a32af
cornixsenex <phallusferrus@toke.com>
2025-01-12 13:10:16 -0800

    added pull tiles for vegas tunnel

--- src/data/items.h
+++ src/data/items.h
@@ -12261,7 +12261,7 @@ const struct Item gItemsInfo[] =
         .importance = 1,
         .pocket = POCKET_KEY_ITEMS,
         .type = ITEM_USE_FIELD,
-        .fieldUseFunc = ItemUseOutOfBattle_CannotUse,
+        .fieldUseFunc = ItemUseOutOfBattle_Bike,
         .iconPic = gItemIcon_Bicycle,
         .iconPalette = gItemIconPalette_Bicycle,
     },
