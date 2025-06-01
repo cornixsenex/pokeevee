1321d81691d562a2ebffc10657944eafca6d4518
cornixsenex <phallusferrus@toke.com>
2025-05-31 22:08:02 -0700

    everything even dns seems working

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -100,7 +100,7 @@
 #define OW_ENABLE_DNS                   TRUE    // If set to TRUE, the overworld will be tinted depending on time of day.
 
 // Object Event Shadows
-#define OW_OBJECT_VANILLA_SHADOWS      TRUE    // In vanilla shadows in the overworld are only shown when jumping.
+#define OW_OBJECT_VANILLA_SHADOWS      FALSE    // In vanilla shadows in the overworld are only shown when jumping.
 
 // Overworld flags
 // To use the following features in scripting, replace the 0s with the flag ID you're assigning it to.
