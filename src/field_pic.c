a0aa2221eb18ee55ac88958651733dcb219fe1ac
cornixsenex <phallusferrus@toke.com>
2025-07-15 12:25:35 -0700

    work on mares9 aeneid scene

--- src/field_pic.c
+++ src/field_pic.c
@@ -168,6 +168,10 @@ static const u16 sPic34Pal[] = INCBIN_U16("graphics/trainers/palettes/scott.gbap
 static const u32 sPic35Gfx[] = INCBIN_U32("graphics/trainers/front_pics/misty2_front_pic.4bpp.lz");
 static const u16 sPic35Pal[] = INCBIN_U16("graphics/trainers/palettes/misty2.gbapal");
 
+//Lenora = 35
+static const u32 sPic36Gfx[] = INCBIN_U32("graphics/trainers/front_pics/lenora.4bpp.lz");
+static const u16 sPic36Pal[] = INCBIN_U16("graphics/trainers/palettes/lenora.gbapal");
+
 
 
 /* And this is an example script.
@@ -263,6 +267,8 @@ static const struct Pic sPics[] =
     {sPic34Gfx, sPic34Pal, SPRITE_SHAPE(64x64), SPRITE_SIZE(64x64)},
 	//Misty
     {sPic35Gfx, sPic35Pal, SPRITE_SHAPE(64x64), SPRITE_SIZE(64x64)},
+	//Lenora
+    {sPic36Gfx, sPic36Pal, SPRITE_SHAPE(64x64), SPRITE_SIZE(64x64)},
 };
 
 static EWRAM_DATA u8 sLastPicId = 0;
