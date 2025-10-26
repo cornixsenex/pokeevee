2f80aa0355698163c74a78896d8214ab670fc976
cornixsenex <phallusferrus@toke.com>
2025-10-25 20:50:30 -0700

    work on lotus eaters scene

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -136,7 +136,7 @@
 #define VAR_ULYSSES_STATE                                0x4059 // Track Ulysses thru Odyssey
 																// 0: Ulyssas on Ithaca - either in house or "mad"
 																// 1: Telemachus scene (On Frame in MareS7 et Casa do Odyssey begin scene)
-                                                                // 2: Ulysses Lotus Episode
+                                                                // 2: Ulysses and Lotus Eaters MareS3
 #define VAR_RUSTBORO_CITY_STATE                          0x405A
 #define VAR_FORTREE_CITY_STATE                           0x405B // Unused Var
 #define VAR_LILYCOVE_CITY_STATE                          0x405C // Unused Var
